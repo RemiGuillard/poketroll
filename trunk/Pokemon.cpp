55e994bed9453de9b5e1d7dc3db06162e03b0be9
lolokun <lolokun@f8136148-a51c-7980-e803-3b4f71785a99>
2010-11-13 11:51:56 +0000

    std::string => QString + remplissage attributs Pokemon (readName)

--- trunk/Pokemon.cpp
+++ trunk/Pokemon.cpp
@@ -1,11 +1,12 @@
+#include <QString>
 #include "Pokemon.h"
 
 Pokemon::Pokemon() {}
 
-Pokemon::Pokemon(int id, string name, list<string> types, string ability,
+Pokemon::Pokemon(int id, QString name, list<QString> types, QString ability,
 			int exp/*, Stats* stats*//*, list<Evolution*> evolutions*/,
-			float ratio, list<string> eggGroup, string species, float height,
-			float weight, string description/*, list<Attack *> attacks*/)
+			float ratio, list<QString> eggGroup, QString species, float height,
+			float weight, QString description/*, list<Attack *> attacks*/)
 			: _id(id), _name(name), _types(types), _ability(ability),
 			_exp(exp)/*, _stats(stats)*/,/* _evolutions(evolutions),*/
 			_ratio(ratio), _eggGroup(eggGroup), _species(species), _height(height),
@@ -42,11 +43,11 @@ Pokemon&			Pokemon::operator=(const Pokemon &cpy)
 
 int					Pokemon::getId() const { return _id; }
 
-string				Pokemon::getName() const { return _name; }
+QString				Pokemon::getName() const { return _name; }
 
-list<string>		Pokemon::getTypes() const { return _types; }
+list<QString>		Pokemon::getTypes() const { return _types; }
 
-string				Pokemon::getAbility() const { return _ability; }
+QString				Pokemon::getAbility() const { return _ability; }
 
 int					Pokemon::getExp() const { return _exp; }
 
@@ -56,25 +57,25 @@ int					Pokemon::getExp() const { return _exp; }
 
 float				Pokemon::getRatio() const { return _ratio; }
 
-list<string>		Pokemon::getEggGroup() const { return _eggGroup; }
+list<QString>		Pokemon::getEggGroup() const { return _eggGroup; }
 
-string				Pokemon::getSpecies() const { return _species; }
+QString				Pokemon::getSpecies() const { return _species; }
 
 float				Pokemon::getHeight() const { return _height; }
 
 float				Pokemon::getWeight() const { return _weight; }
 
-string				Pokemon::getDescription() const { return _description; }
+QString				Pokemon::getDescription() const { return _description; }
 
 //list<Attack *>		Pokemon::getAttacks() const { return _attacks; }
 
 void				Pokemon::setId(int id) { _id = id; }
 
-void				Pokemon::setName(string name) { _name = name; }
+void				Pokemon::setName(QString name) { _name = name; }
 
-void				Pokemon::setTypes(list<string> types) { _types = types; }
+void				Pokemon::setTypes(list<QString> types) { _types = types; }
 
-void				Pokemon::setAbility(string abi) { _ability = abi; }
+void				Pokemon::setAbility(QString abi) { _ability = abi; }
 
 void				Pokemon::setExp(int exp) { _exp = exp; }
 
@@ -84,14 +85,14 @@ void				Pokemon::setExp(int exp) { _exp = exp; }
 
 void				Pokemon::setRatio(float ratio) { _ratio = ratio; }
 
-void				Pokemon::setEggGroup(list<string> egg) { _eggGroup = egg; }
+void				Pokemon::setEggGroup(list<QString> egg) { _eggGroup = egg; }
 
-void				Pokemon::setSpecies(string spec) { _species = spec; }
+void				Pokemon::setSpecies(QString spec) { _species = spec; }
 
 void				Pokemon::setHeight(float h) { _height = h; }
 
 void				Pokemon::setWeight(float w) { _weight = w; }
 
-void				Pokemon::setDescription(string desc) { _description = desc; }
+void				Pokemon::setDescription(QString desc) { _description = desc; }
 
 //void				Pokemon::setAttacks(list<Attacks *> att) { _attacks = att; }
