f16ad5c1ccd84044c263711eebfb576d65bfc875
remi.rhcp <remi.rhcp@f8136148-a51c-7980-e803-3b4f71785a99>
2010-11-13 14:26:37 +0000

    Ajout des Classes Attack et Evolution

---
+++ trunk/Attack.h
@@ -0,0 +1,33 @@
+#ifndef ATTACK
+# define ATTACK
+
+class		Attack
+{
+public:
+	Attack();
+	Attack(QString, QString, QString = "", QString = "");
+	Attack(const Attack&);
+	~Attack();
+
+	Attack&			operator=(const Attack&);
+
+	QString			getName() const;
+	void			setName(QString);
+
+	QString			getType() const;
+	void			setType(QString);
+
+	QString			getMachine() const;
+	void			setMachine(QString);
+
+	QString			getLvl() const;
+	void			setRequire(QString);
+
+private:
+	QString			_type;
+	QString			_name;
+	QString			_machine;
+	QString			_lvl;
+};
+
+#endif // !ATTACK
