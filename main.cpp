6ed08851239889819c04b175e6cbf83d07affec6
lolokun <lolokun@f8136148-a51c-7980-e803-3b4f71785a99>
2010-11-14 02:23:34 +0000

    fin etape 1

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include "pokedex.h"
 #include <QtGui/QApplication>
+ #include <QSplashScreen>
+ #include <QTest>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	Pokedex w;
+	QSplashScreen	splash(QPixmap("pictures/1.png"));
 	//start load xml
 	//put all pokemon in window
+	splash.show();
+	QTest::qSleep(2000);
 	w.pokemonNameListDisplay();
 	w.pokemonDisplay(w.getPokeList().value(1));
+	//splash.close();
 	w.show();
 	return a.exec();
 }
